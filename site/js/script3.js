eeb5a638950ccc614a99144676391d1e1fb82f18
Dmitriy Trifonov <yumee.dev@gmail.com>
2021-11-06 15:21:22 +0300

    Test AJAX with JSON data

--- site/js/script3.js
+++ site/js/script3.js
@@ -1,11 +1,12 @@
 {
 	document.addEventListener("DOMContentLoaded", function (event) {
 		document.querySelector("h1").addEventListener("click", function() {
-			$ajaxUtils.sendGetRequest("data/simple text.txt", h1clickhandler);
+			$ajaxUtils.sendGetRequest("data/json text.json", h1clickhandler);
 		});
 	});
 
 	function h1clickhandler(request) {
-		document.querySelector("h1").textContent = request.responseText;
+		document.querySelector("h1").textContent = "First name is " + (JSON.parse(request.responseText)).first-name +
+		". Last name is " + (JSON.parse(request.responseText)).last-name;
 	}
 }
