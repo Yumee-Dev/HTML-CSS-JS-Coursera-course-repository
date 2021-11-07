0b05ced717482dfc1727fe101c83798770c05e8c
Dmitriy Trifonov <yumee.dev@gmail.com>
2021-11-07 17:51:30 +0300

    Test dynamic page generating with JSON array

--- site/js/script3.js
+++ site/js/script3.js
@@ -1,7 +1,28 @@
 {
-	document.addEventListener("DOMContentLoaded", function (event) {
+	$(function (event) {
 		document.querySelector("h1").addEventListener("click", function() {
 			$ajaxUtils.sendGetRequest("data/json text.json", h1clickhandler);
+		});
+	});
+
+	$(function (event){
+		$("a").click(function (event) {
+			$ajaxUtils.sendGetRequest("snippets/single-tile.html", function(singleTileHTMLreq) {
+				$ajaxUtils.sendGetRequest("data/tiles-content.json", function(tilesContentreq) {
+					var singleTileHTML = singleTileHTMLreq.responseText;
+					var tilesContent = JSON.parse(tilesContentreq.responseText);
+					var finalHTML = "<section class='row'>";
+					for (i = 0; i < tilesContent.length; i++) {
+						var tileHTML = singleTileHTML;
+						tileHTML = tileHTML.replace(new RegExp("{{LETTER}}", "g"), tilesContent[i].letter);
+						tileHTML = tileHTML.replace(new RegExp("{{DESCRIPTION}}", "g"), tilesContent[i].description);
+						tileHTML = tileHTML.replace(new RegExp("{{TITLE}}", "g"), tilesContent[i].title);
+						finalHTML += tileHTML;
+					}
+					finalHTML += "</section>";
+					document.querySelector("#all-content-div").innerHTML = finalHTML;
+				});
+			});
 		});
 	});
 
